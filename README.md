7f9242f854aa4583f91a64a4ed888a802e2deaca
misostack <misostack@gmail.com>
2020-07-11 15:52:27 +0700

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Fullstack JS CS1 Application
+
+1. Stylesheets
+
+> Framework : bootstrap
+
+```
+yarn add react-bootstrap bootstrap
+```
+
+> Organize : SMACSS ( Scalable and Modular Architecture for CSS )
+
+![smac css](./documents/assets/smacss.png)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Header</h1>
+        <p>lorem  i[saum</p>
+      </header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </>
+  );
+}
+
+export default App;
