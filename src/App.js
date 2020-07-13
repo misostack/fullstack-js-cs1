4aa7a08f1543b187462f19a07fb69e4137630afb
misostack <misostack.com@gmail.com>
2020-07-13 18:32:20 +0700

    used google fonts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <header>
-        <h1>Header</h1>
+        <h1>Braintree Payments</h1>
         <p>lorem  i[saum</p>
       </header>
       <main>Main</main>
